9c6caf33a356c1453a893122d26093d45607124b
James L <10925058+jamestl90@users.noreply.github.com>
2024-11-24 22:30:24 +0800

    Add namespace to window class

--- GLSetupTest/Window.cpp
+++ GLSetupTest/Window.cpp
@@ -2,89 +2,92 @@
 #include <iostream>
 #include <glad/glad.h> // Include GLAD for OpenGL function loading
 
-Window::Window(int width, int height, const char* title, int targetFPS, int maxFrameRate)
-    : m_width(width), m_height(height), m_window(nullptr)
+namespace JLEngine
 {
-    // Initialize GLFW and create the window in the constructor
-    if (!init(title)) {
-        std::cerr << "Failed to initialize window!" << std::endl;
-        exit(EXIT_FAILURE);
-    }
-}
-
-Window::~Window()
-{
-    // Clean up GLFW resources
-    if (m_window) {
-        glfwDestroyWindow(m_window);
-    }
-    glfwTerminate();
-}
-
-bool Window::init(const char* name)
-{
-    // Initialize GLFW
-    if (!glfwInit()) {
-        std::cerr << "GLFW initialization failed!" << std::endl;
-        return false;
+    Window::Window(int width, int height, const char* title, int targetFPS, int maxFrameRate)
+        : m_width(width), m_height(height), m_window(nullptr)
+    {
+        // Initialize GLFW and create the window in the constructor
+        if (!init(title)) {
+            std::cerr << "Failed to initialize window!" << std::endl;
+            exit(EXIT_FAILURE);
+        }
     }
 
-    glfwWindowHint(GLFW_SAMPLES, 4); // msaa samples
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6); 
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  
-
-    // Create a GLFW windowed mode window and its OpenGL context
-    m_window = glfwCreateWindow(m_width, m_height, name, nullptr, nullptr);
-    if (!m_window) {
-        std::cerr << "Failed to create GLFW window!" << std::endl;
+    Window::~Window()
+    {
+        // Clean up GLFW resources
+        if (m_window) {
+            glfwDestroyWindow(m_window);
+        }
         glfwTerminate();
-        return false;
     }
 
-    // Make the OpenGL context current
-    glfwMakeContextCurrent(m_window);
+    bool Window::init(const char* name)
+    {
+        // Initialize GLFW
+        if (!glfwInit()) {
+            std::cerr << "GLFW initialization failed!" << std::endl;
+            return false;
+        }
 
-    // Initialize GLAD (OpenGL function loader)
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cerr << "Failed to initialize OpenGL loader!" << std::endl;
-        return false;
+        glfwWindowHint(GLFW_SAMPLES, 4); // msaa samples
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+        // Create a GLFW windowed mode window and its OpenGL context
+        m_window = glfwCreateWindow(m_width, m_height, name, nullptr, nullptr);
+        if (!m_window) {
+            std::cerr << "Failed to create GLFW window!" << std::endl;
+            glfwTerminate();
+            return false;
+        }
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(m_window);
+
+        // Initialize GLAD (OpenGL function loader)
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            std::cerr << "Failed to initialize OpenGL loader!" << std::endl;
+            return false;
+        }
+
+        std::cout << "OpenGL " << glGetString(GL_VERSION) << " initialized!" << std::endl;
+
+        // Set the frame buffer size callback for resizing the window
+        glfwSetFramebufferSizeCallback(m_window, [](GLFWwindow* window, int width, int height) {
+            glViewport(0, 0, width, height);
+            });
+
+        // Set up V-Sync (we can turn it off to allow uncapped rendering if needed)
+        glfwSwapInterval(1);  // Enable V-Sync by default, set to 0 for uncapped
+
+        return true;
     }
 
-    std::cout << "OpenGL " << glGetString(GL_VERSION) << " initialized!" << std::endl;
+    int Window::getWidth() const
+    {
+        return m_width;
+    }
 
-    // Set the frame buffer size callback for resizing the window
-    glfwSetFramebufferSizeCallback(m_window, [](GLFWwindow* window, int width, int height) {
-        glViewport(0, 0, width, height);
-        });
+    int Window::getHeight() const
+    {
+        return m_height;
+    }
 
-    // Set up V-Sync (we can turn it off to allow uncapped rendering if needed)
-    glfwSwapInterval(1);  // Enable V-Sync by default, set to 0 for uncapped
+    bool Window::shouldClose() const
+    {
+        return glfwWindowShouldClose(m_window);
+    }
 
-    return true;
-}
+    void Window::swapBuffers()
+    {
+        glfwSwapBuffers(m_window);
+    }
 
-int Window::getWidth() const
-{
-    return m_width;
-}
-
-int Window::getHeight() const
-{
-    return m_height;
-}
-
-bool Window::shouldClose() const
-{
-    return glfwWindowShouldClose(m_window);
-}
-
-void Window::swapBuffers()
-{
-    glfwSwapBuffers(m_window);
-}
-
-void Window::pollEvents()
-{
-    glfwPollEvents();
+    void Window::pollEvents()
+    {
+        glfwPollEvents();
+    }
 }
--- GLSetupTest/Window.h
+++ GLSetupTest/Window.h
@@ -5,23 +5,25 @@
 #include <GLFW/glfw3.h>
 #include <functional>
 
-class Window
+namespace JLEngine
 {
-public:
-    // Constructor to create the window
-    Window(int width, int height, const char* title, int targetFPS = 60, int maxFrameRate = 0);
-    ~Window();
+    class Window
+    {
+    public:
+        // Constructor to create the window
+        Window(int width, int height, const char* title, int targetFPS = 60, int maxFrameRate = 0);
+        ~Window();
 
-    bool init(const char* name);
-    int getWidth() const;
-    int getHeight() const;
-    bool shouldClose() const;
-    void swapBuffers();
-    void pollEvents();
+        bool init(const char* name);
+        int getWidth() const;
+        int getHeight() const;
+        bool shouldClose() const;
+        void swapBuffers();
+        void pollEvents();
 
-private:
-    GLFWwindow* m_window;
-    int m_width, m_height; 
-};
-
+    private:
+        GLFWwindow* m_window;
+        int m_width, m_height;
+    };
+}
 #endif
